2fb80eb4cccaafd540217f5e0e8f30a7bf7fbcdf
Vladimir Melnik <melnik.wladimir@gmail.com>
2018-08-17 12:23:14 +0300

    cleanup code

--- tests/Connector/DSNConnectorTest.php
+++ tests/Connector/DSNConnectorTest.php
@@ -2,39 +2,7 @@
 
 class DSNConnectorTest extends \PHPUnit_Framework_TestCase
 {
-    private $connectData = [];
     private $connector;
-    
-    /**
-     * @inheritdoc
-     */
-    public function connect(array $options)
-    {
-        $this->connectData["dsn"] = $options["dsn"];
-        $this->connectData["username"] = isset($options["username"]) ? $options["username"] : null;
-        $this->connectData["password"] = isset($options["password"]) ? $options["password"] : null;
-        $this->connectData["driver_options"] = isset($options["driver_options"]) ? $options["driver_options"] : [];
-        
-        return new \PDO(
-            $this->connectData["dsn"],
-            $this->connectData["username"],
-            $this->connectData["password"],
-            $this->connectData["driver_options"]
-        );
-    }
-    
-    public function supports($options)
-    {
-        if (!is_array($options)) {
-            return false;
-        }
-    
-        if (!array_key_exists("dsn", $options)) {
-            return false;
-        }
-        
-        return true;
-    }
     
     public function setUp()
     {
