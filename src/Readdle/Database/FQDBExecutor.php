761366ec09af0c376f6a876a7a11006f084c3623
Kyrylo Meretskyi <kmeretskyi@readdle.com>
2025-01-22 14:06:18 +0200

    SI-1917 - linter fix

--- src/Readdle/Database/FQDBExecutor.php
+++ src/Readdle/Database/FQDBExecutor.php
@@ -242,7 +242,7 @@ class FQDBExecutor implements FQDBInterface
             $this->lastCheckTime = \time();
             
             if ($needsLastInsertId) {
-                $lastInsertId = (int) $this->pdo->lastInsertId(); // if table has no AUTO_INCREMENT PRI KEY, there will be 0
+                $lastInsertId = (int)$this->pdo->lastInsertId(); // if table has no AUTO_INCREMENT PRI KEY, there will be 0
             }
         } catch (\PDOException $e) {
             $this->error(FQDBException::pdo($e, ["query" => $query, "params" => $params]));
